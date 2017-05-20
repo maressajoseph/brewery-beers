272d80256c674f970bf4419cbe398ab84000cad6
maressajoseph <maressa.joseph@gmail.com>
2017-05-20 13:09:08 +0200

    New built

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -6,4 +6,20 @@ class BreweriesController < ApplicationController
   def show
     @brewery = Brewery.find(params[:id])
   end
+
+  def new
+    @brewery = Brewery.new
+  end
+
+  def create
+    brewery_params = params.require(:brewery).permit(:name, :location, :img_url)
+
+    @brewery = Brewery.new(brewery_params)
+
+    if @brewery.save
+       redirect_to @brewery
+    else
+       render 'new'
+    end
+  end
 end
